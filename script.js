9865af80e9664ece6dbaff48f4e7eb69de84776c
munyakarg <108096449+munyakarg@users.noreply.github.com>
2026-02-05 14:45:18 -0800

    update location again for boxes

--- script.js
+++ script.js
@@ -193,7 +193,7 @@
                     
                     const popupContent = `
                         <div class="popup-name">${loc.name}</div>
-                        ${loc.location ? `<div class="popup-detail">📍 ${loc.location}</div>` : ''}
+                        ${loc.location ? `<div class="popup-detail">📍 ${loc.location}, ${loc.city}, ${loc.state}, ${loc.zip}</div>` : ''}
                         ${hoursHTML}
                         ${loc.type ? `<div class="popup-detail">🏪 ${loc.type}</div>` : ''}
                     `;
